e1c7db8e179ab9b49eb412207970a66522c16407
auifzysr <auifzysr@gmail.com>
2020-12-30 20:56:56 +0900

    parameterize

--- cmd/envii_exporter/main.go
+++ cmd/envii_exporter/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"flag"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/auifzysr/envii_exporter/pkg/exporter"
 	"github.com/go-kit/kit/log/level"
+	"github.com/jamiealquiza/envy"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/common/promlog"
@@ -16,22 +18,23 @@ import (
 var metricsPath = "/metrics"
 
 func main() {
-	// TOOD: parameterize
-	address := "http://192.168.11.12:9000"
-	timeout := time.Duration(5)
-	logger := promlog.New(&promlog.Config{})
-	listenAddress := "0.0.0.0:8001"
+	var poll_address = flag.String("poll_address", "localhost:9000", "The address to which the exporter access to get metrics.")
+	var timeout = flag.Int("timeout", 15, "Seconds the exporter waits to get response.")
+	var listen_address = flag.String("listen_address", "0.0.0.0:8001", "The address on which the exporter listens for connections.")
+	envy.Parse("ENVII")
+	flag.Parse()
 
+	var logger = promlog.New(&promlog.Config{})
 	level.Info(logger).Log("msg", "Starting memcached_exporter", "version", version.Info())
 	level.Info(logger).Log("msg", "Build context", "context", version.BuildContext())
 
 	prometheus.MustRegister(version.NewCollector("envii_exporter"))
-	prometheus.MustRegister(exporter.New(address, timeout, logger))
+	prometheus.MustRegister(exporter.New(*poll_address, time.Duration(*timeout), logger))
 
 	http.Handle(metricsPath, promhttp.Handler())
 
-	level.Info(logger).Log("msg", "Listening on address", "address", listenAddress)
-	if err := http.ListenAndServe(listenAddress, nil); err != nil {
+	level.Info(logger).Log("msg", "Listening on address", "address", *listen_address)
+	if err := http.ListenAndServe(*listen_address, nil); err != nil {
 		level.Error(logger).Log("msg", "Error running http server", "err", err)
 		os.Exit(1)
 	}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/go-kit/kit v0.10.0
+	github.com/jamiealquiza/envy v1.1.0
 	github.com/pkg/errors v0.9.1
 	github.com/prometheus/client_golang v1.8.0
 	github.com/prometheus/common v0.14.0
