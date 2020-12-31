5607dda33c50dabfd75cbbcc7cd64ff8a8a3268f
auifzysr <auifzysr@gmail.com>
2020-12-31 18:47:56 +0900

    refactoring

--- arduino/atom/envii.ino
+++ arduino/atom/envii.ino
@@ -16,17 +16,20 @@
 #include "Adafruit_Sensor.h"
 #include "SHT3X.h"
 
-const char* ssid = "wifissid";
-const char* password = "wifipassword";
-
-WebServer server(9000);
+const char* WIFI_SSID = "somessid";
+const char* WIFI_PASSWORD = "somepassword";
+const int I2C_SDA = 26;
+const int I2C_SCL = 32;
+const uint32_t I2C_FREQ = 100000;
+const long SERIAL_SPEED = 115200;
+const int WEBSERVER_PORT = 9000;
 
 Adafruit_BMP280 bme;
 SHT3X sht30;
 
-float tmp = 0.0;
-float hum = 0.0;
-float pressure = 0.0;
+float c, h, p = 0.0;
+
+WebServer server(WEBSERVER_PORT);
 
 uint8_t DisBuff[2 + 5 * 5 * 3];
 
@@ -43,75 +46,50 @@ void setBuff(uint8_t Rdata, uint8_t Gdata, uint8_t Bdata)
 }
 
 void handleRoot() {
-  if (sht30.get() == 0) {
-    setBuff(0x00, 0x40, 0x00);
-    M5.dis.displaybuff(DisBuff);
-    M5.update();
-    server.send(200, "text/plain",
-                "{\"c\":" + String(sht30.cTemp)
-                + ",\"h\":" + String(sht30.humidity)
-                + ",\"p\":" + String(bme.readPressure())
-                + "}");
-    return;
-  }
-  setBuff(0x40, 0x00, 0x00);
-  M5.dis.displaybuff(DisBuff);
-  M5.update();
-  server.send(500, "text/plain", "");
+  server.send(200, "text/plain", "{\"c\":" + String(c) + ",\"h\":" + String(h) + ",\"p\":" + String(p) + "}");
 }
 
 void handleNotFound() {
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
+  server.send(404, "text/plain", "");
 }
 
 void setup(void) {
-  Serial.begin(115200);
-  WiFi.begin(ssid, password);
-  Serial.println("");
+  // starts Serial Port
+  Serial.begin(SERIAL_SPEED);
+  M5.begin(true, true, true);
+  Wire.begin(I2C_SDA, I2C_SCL, I2C_FREQ);
+  while (!bme.begin(0x76)) {
+    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+  }
 
-  // Wait for connection
+  // starts WiFi
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.println("");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.printf("Connected to SSID:%s\n", WIFI_SSID);
 
+  // starts HTTP server
   server.on("/", handleRoot);
-
-  server.on("/inline", []() {
-    server.send(200, "text/plain", "this works as well");
-  });
-
   server.onNotFound(handleNotFound);
-
   server.begin();
   Serial.println("HTTP server started");
-
-  M5.begin(true, true, true);
-  Wire.begin(26, 32);
-  Serial.println(F("ENV Unit(SHT30 and BMP280) test..."));
-
-  while (!bme.begin(0x76)) {
-    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
-  }
-
 }
 
 void loop(void) {
+  // gets senser data
+  if (sht30.get() == 0) {
+    c = sht30.cTemp;
+    h = sht30.humidity;
+    p = bme.readPressure();
+    setBuff(0x00, 0x40, 0x00);
+  } else {
+    setBuff(0x40, 0x00, 0x00);
+  }
+
+  M5.dis.displaybuff(DisBuff);
   server.handleClient();
 }
